520d0b335302c6ce8face6520a361407e7e50050
Jonathan Eom <jonnyeom@gmail.com>
2019-10-02 15:46:20 -0400

    Apply input_accepting_both-2916524-4.patch

--- src/Validator.php
+++ src/Validator.php
@@ -41,17 +41,23 @@ class Validator {
    *   Phone number.
    * @param int $format
    *   Supported input format.
-   * @param array $country
+   * @param array $regions
    *   (optional) List of supported countries. If empty all countries are valid.
+   * @param null|string $default_region
+   *   (optional) Default country
    *
    * @return bool
    *   Boolean representation of validation result.
    */
-  public function isValid($value, $format, array $country = []) {
+  public function isValid($value, $format, array $regions = [], $default_region = NULL) {
 
     try {
-      // Get default country.
-      $default_region = ($format == PhoneNumberFormat::NATIONAL) ? reset($country) : NULL;
+      // Get default region.
+      // If default region is not provided and national format in use,
+      // we must assume default region.
+      if (!isset($default_region) && $format == PhoneNumberFormat::NATIONAL) {
+        $default_region = reset($regions);
+      }
       // Parse to object.
       $number = $this->phoneUtils->parse($value, $default_region);
     }
@@ -65,12 +71,12 @@ class Validator {
       return FALSE;
     }
 
-    // If country array is not empty and default region can be loaded
+    // If country array is not empty and default country can be loaded
     // do region matching validation.
     // This condition is always TRUE for national phone number format.
-    if (!empty($country) && $default_region = $this->phoneUtils->getRegionCodeForNumber($number)) {
+    if (!empty($regions) && (empty($default_region) || $regions = array_merge($regions, [$default_region => $default_region])) && $default_region = $this->phoneUtils->getRegionCodeForNumber($number)) {
       // Check if number's region matches list of supported countries.
-      if (array_search($default_region, $country) === FALSE) {
+      if (array_search($default_region, $regions) === FALSE) {
         return FALSE;
       }
     }
